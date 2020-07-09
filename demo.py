89031b36b333b836ef5a13a708c1cd88debd62fd
Codefreak3 <68070124+Codefreak3@users.noreply.github.com>
2020-07-09 16:04:04 +0100

    Update demo.py
    adding one line on contribution

--- demo.py
+++ demo.py
@@ -4,3 +4,4 @@ print(" Happy to interact with you! ")
 print("Happy to see your message")
 print("I can see the change you pushed onto the remote repository from the local one")
 
+print("I want to contribute to the project")
