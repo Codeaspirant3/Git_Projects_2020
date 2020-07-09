fc25b9cb49bfb07631e5b6463ebb53f7f448b616
Codeaspirant3 <68069227+Codeaspirant3@users.noreply.github.com>
2020-07-09 15:34:33 +0100

    Update demo.py
    one line added

--- demo.py
+++ demo.py
@@ -2,5 +2,5 @@ print ("Hello World")
 print(" Happy to interact with you! ")
 
 print("Happy to see your message")
-
+print("I can see the change you pushed onto the remote repository from the loal one")
 
